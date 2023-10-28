cccf9b42ff64c243a116212c6742745b9151a04b
Sarthak Rai <106958899+sarthak2685@users.noreply.github.com>
2023-10-28 13:15:06 +0530

    Create reverselinllist.cpp

---
+++ Linked List/reverselinllist.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() {
+        head = nullptr;
+    }
+
+    // Function to insert a new node at the end of the linked list
+    void insert(int value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            Node* current = head;
+            while (current->next != nullptr) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+
+    // Function to reverse the linked list
+    void reverse() {
+        Node* prev = nullptr;
+        Node* current = head;
+        Node* next = nullptr;
+
+        while (current != nullptr) {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+
+        head = prev;
+    }
+
+    // Function to display the linked list
+    void display() {
+        Node* current = head;
+        while (current != nullptr) {
+            std::cout << current->data << " -> ";
+            current = current->next;
+        }
+        std::cout << "nullptr" << std::endl;
+    }
+};
+
+int main() {
+    LinkedList linkedList;
+
+    linkedList.insert(1);
+    linkedList.insert(2);
+    linkedList.insert(3);
+
+    std::cout << "Original Linked List: ";
+    linkedList.display();
+
+    linkedList.reverse();
+
+    std::cout << "Reversed Linked List: ";
+    linkedList.display();
+
+    return 0;
+}
